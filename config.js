d1dceca1a90b7047f32f505683f0be014a3257c8
Minminn01 <86761850+Minminn01@users.noreply.github.com>
2021-07-01 12:18:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Halo bbi',
+    introDesc: `Hú hồn chưa`,
+    btnIntro: 'Tiếp nhó :3',
+    title: 'Anh bé cũng iu em mà đúng hôn 🥺',
+    desc: 'Thử nói hong coi ',
+    btnYes: 'Đúng gòiii',
+    btnNo: 'Hông luôn á',
+    question:'Sao bbi iu em á nhỉ 😳',
+    btnReply: 'Đây đây',
+    reply: 'Tại vì em đáng iu ó',
+    mess: 'Chòi oi em biết mà, nói dị ngại ghe 🤭',
+    messDesc: 'Chúc bbi thi tốt nhó, xíu nữa hoii, cố lên nhó 😉',
+    btnAccept: 'Ukii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
